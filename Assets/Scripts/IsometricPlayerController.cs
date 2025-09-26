bdd392ad7cf215211fe29efcaf07c3931bd66405
Gary DiCiano <garydiciano4@gmail.com>
2025-09-26 07:49:08 -0400

    Corrected logic in sprite flip
    The player would always look to the left even if the last input they registered was moving right. This has been corrected and the code has been simplified.

--- Assets/Scripts/IsometricPlayerController.cs
+++ Assets/Scripts/IsometricPlayerController.cs
@@ -8,14 +8,12 @@ public class IsometricPlayerController : MonoBehaviour
     [SerializeField] Rigidbody2D rb;
     [SerializeField] SpriteRenderer sr;
 
-    [SerializeField] private float moveSpeed;
     [SerializeField] private Vector2 rawMoveVector;
     [SerializeField] private Vector2 moveVector;
     [SerializeField] private Vector2 normalizedMoveVector;
+    [SerializeField] private float moveSpeed;
     [SerializeField] private int xDirection;
-    [SerializeField] private int lastXDirection;
     [SerializeField] private int yDirection;
-    [SerializeField] private int lastYDirection;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -25,7 +23,6 @@ public class IsometricPlayerController : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
         sr = GetComponent<SpriteRenderer>();
     }
-
 
     // Update is called once per frame
     void Update()
@@ -38,20 +35,14 @@ public class IsometricPlayerController : MonoBehaviour
         xDirection = Mathf.RoundToInt(normalizedMoveVector.x);
         yDirection = Mathf.RoundToInt(normalizedMoveVector.y);
 
-        if (xDirection != lastXDirection) lastXDirection = xDirection;
-        if (yDirection != lastYDirection) lastYDirection = yDirection;
-
         animator.SetInteger("X-Direction", xDirection);
-        animator.SetInteger("Last-X-Direction", lastXDirection);
         animator.SetInteger("Y-Direction", yDirection);
-        animator.SetInteger("Last-Y-Direction", lastYDirection);
 
         if (xDirection > 0)
             sr.flipX = true;
-        else
+        else if (xDirection < 0)
             sr.flipX = false;
-
-        // transform.Translate(moveVector * moveSpeed * Time.deltaTime);
+        
         rb.linearVelocity = (normalizedMoveVector * moveSpeed); 
     }
 }
